efc7e7403dee378d2e5f31edf77d3fb5813b855a
Mateus Cintra <101955322+cintra1@users.noreply.github.com>
2024-02-03 20:15:55 -0300

    Adicionando arquivos

---
+++ FunctionCelsiusParaFahrenheit.cs
@@ -0,0 +1,44 @@
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public class FunctionCelsiusParaFahrenheit
+    {
+        private readonly ILogger<FunctionCelsiusParaFahrenheit> _logger;
+
+        public FunctionCelsiusParaFahrenheit(ILogger<FunctionCelsiusParaFahrenheit> log)
+        {
+            _logger = log;
+        }
+
+        [FunctionName("ConverterCelsiusParaFahrenheit")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "Conversão" })]
+        [OpenApiParameter(name: "celsius", In = ParameterLocation.Path, Required = true, Type = typeof(double), Description = "O valor em celsius para conversão em fahrenheit")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "Retorna o valor em Fahrenheit")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ConverterCelsiusParaFahrenheit/{celsius}")] HttpRequest req,
+            double celsius)
+        {
+            _logger.LogInformation($"Parâmetro recebido: {celsius}", celsius);
+
+            var valorEmFahrenheit = ((celsius * 9) / 5) + 32;
+
+            string responseMessage = $"O valor em Celsius {celsius} em Fahrenheit é: {valorEmFahrenheit}";
+
+            _logger.LogInformation($"Conversão efetuada: {valorEmFahrenheit}");
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
+
